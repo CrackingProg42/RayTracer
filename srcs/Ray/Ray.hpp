15f68c96283533ecabff0d26f30b08b2a97ab44d
CrackingProg42 <quentinfeuillademontixi42@gmail.com>
2019-12-11 13:24:44 +0100

    add Ray class

---
+++ srcs/Ray/Ray.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Ray.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: QFM <quentin.feuillade33@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/11 12:46:25 by QFM               #+#    #+#             */
+/*   Updated: 2019/12/11 13:01:14 by QFM              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAY_HPP
+#define RAY_HPP
+
+#include "../global.hpp"
+
+class Ray
+{
+private:
+
+	Vector pos;
+	Vector dir;
+
+public:
+
+	Ray();
+	Ray(Vector pos, Vector dir);
+	~Ray();
+	Ray(Ray const &);
+
+};
+
+#endif
