8083e0f92f949d1e59f473f41167eea42d53e446
CrackingProg42 <quentinfeuillademontixi42@gmail.com>
2019-12-11 22:40:26 +0100

    adding lib math

--- srcs/global.hpp
+++ srcs/global.hpp
@@ -6,7 +6,7 @@
 /*   By: QFM <quentin.feuillade33@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/08 13:35:02 by QFM               #+#    #+#             */
-/*   Updated: 2019/12/11 13:49:21 by QFM              ###   ########.fr       */
+/*   Updated: 2019/12/11 16:26:24 by QFM              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,11 @@
 #include "Material/Material.hpp"
 #include <fstream>
 #include <string.h>
+#include <cmath>
 
 #define IMAGE_W 500
 #define IMAGE_H 500
 
-extern std::map<int, Vector>				g_normal;				// must start at index 1 !!
-extern std::map<int, Vector>				g_uvs;					// must start at index 1 !!
-extern std::map<int, Vector>				g_vertex;				// must start at index 1 !!
 extern std::map<std::string, Material> 	materials;					// list of all material
 int			material_parser(std::string	mtl_name);
 
